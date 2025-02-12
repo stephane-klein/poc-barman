a3cd0ddf76f57a7effde7f624e0ec317276bd8eb
stephane-klein <contact@stephane-klein.info>
2025-02-12 23:02:26 +0100

    Deleting files that have been copied here by mistake

--- generate_dummy_rows.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-cd "$(dirname "$0")/../"
-
-export PGPASSWORD=password
-psql -n -q -d postgres -h localhost -p 5432 -U postgres -c "select insert_dummy_records(10); select * from dummy order by id desc limit 1"
